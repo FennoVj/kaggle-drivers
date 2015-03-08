Removed files starting with 'fenno' (sorry for narcissism), fixed bug in feature calculation

simplePreprocess/extractSimpleFeatures.py
@@ -1,19 +1,9 @@
-
 import numpy as np
 
-from pyprocessor import trip, full_data
+from simplePyprocessor import trip
+from simpleFeatures import features
 
-
-total_time = lambda trip: trip.n
-total_distance = lambda trip: trip.distance_driven
-straight_distance = lambda trip: trip.distance_straight
-straightness = lambda trip: trip.straightness
-sum_turnspeeds = lambda trip: sum(trip.s(trip.t)/trip.v(trip.t))**2
-acceleration_to_dist = lambda trip: sum(trip.a(trip.t)**2)
-
-
-
-features = [total_time, total_distance, straight_distance, straightness, sum_turnspeeds, acceleration_to_dist]
+full_data = '../data/drivers'
 
 def main(driver_id):
     from os import listdir, path
@@ -24,12 +14,11 @@ def main(driver_id):
     feats = np.empty((len(tripfiles), len(features)))
 
     #The following 5 lines are added by fenno to fix a bug in the code
-    driverfolder = tripfiles[0][:-5] #kinda dirty hack, only works if 1.csv is first file in folder
+    driverfolder = path.dirname(tripfiles[0]) + '/'
     basenames = [path.basename(f) for f in tripfiles]
     basenames = [int(f[:-4]) for f in basenames]
     basenames =  [str(f) + '.csv' for f in sorted(basenames)]
     tripfiles = [driverfolder + b for b in basenames]
-
 
     for i, file in enumerate(tripfiles):
         t = trip(file)
@@ -49,3 +38,9 @@ if __name__=='__main__':
 
     folder = argv[1]
     main(folder)
+
+        #sample usage:   
+        #parallel -j 24 python ./extractSimpleFeatures.py -- `ls ../data/drivers`
+        #rm features.zip
+        #cd fennoOutput
+        #zip -r ../features.zip *

simplePreprocess/simpleFeatures.py
@@ -1,4 +1,3 @@
-
 """
 Created on Sat Mar 07 13:05:04 2015
 
@@ -6,14 +5,14 @@ Created on Sat Mar 07 13:05:04 2015
 """
 import numpy as np
 
-from fennoSimplePyprocessor import trip
+from simplePyprocessor import trip
 
 """
 Threshold Feature
 Takes only elements that exceed threshold, and does an operation on them to get final feature
 """
 def thF(array, operation = np.mean, threshold = 0, greater = True):
-    values = (array > threshold) if greater else (array < threshold)
+    values = array[(array > threshold)] if greater else array[(array < threshold)]
     return operation(values) 
 """
 Count Feature
@@ -40,7 +39,8 @@ acceleration_to_dist = lambda trip: np.sum(trip.a**2)
 #Hue's ideas + enhancements by Fenno
 mean_acceleration = lambda threshold: lambda trip: thF(trip.a, np.mean, threshold, True)#sample threshold: 0
 mean_decceleration = lambda threshold: lambda trip: thF(trip.a, np.mean, threshold, False) #sample threshold: 0
-total_standstill_time = lambda threshold: lambda trip: coF(trip.v < threshold) #sample threshold: 0.1
+total_standstill_time = lambda threshold: lambda trip: coF(trip.v < threshold, False) #sample threshold: 0.1
+proportion_standstill_time =  lambda threshold: lambda trip: coF(trip.v < threshold, True) 
 
 #Fenno's ideas
 turnspeed_velocity = lambda trip: np.sum(trip.s * trip.v)